d99bee10f1ed69a18d159f21698b8b10e37f042d
tomeytran <tmtri310251@gmail.com>
2024-10-10 21:48:30 +0700

    promax

--- src/CheckNum.java
+++ src/CheckNum.java
@@ -2,75 +2,87 @@ import java.util.Scanner;
 
 public class CheckNum {
     public static void main(String[] args) {
-        // Bai 1
         Scanner input = new Scanner(System.in);
-        System.out.println("Menu");
-        System.out.println("1. Tinh tong so le");
-        System.out.println("2. Kiem tra so nguyen to");
-        System.out.println("3. Tinh so ngay moi thang");
-        System.out.print("Chon chuong trinh: ");
+        String continueChoice;
 
-        int option = input.nextInt();
+        do {
+            System.out.println("Programs:");
+            System.out.println("1. Sum of odd numbers");
+            System.out.println("2. Is the number prime?");
+            System.out.println("3. Number of Days in a month");
+            System.out.print("Select an option: ");
 
-        switch (option){
-            case 1:
-                System.out.print("Nhap so de tinh tong so le: ");
-                int num = input.nextInt();
-                int sum = 0;
-                if (num > 10 && num < 100) {
-                    for (int i = 1; i <= num; i++){
-                        if (i % 2 != 0){
-                            sum += i;
+            int option = input.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.print("Pick a number from 11-99: ");
+                    int num = input.nextInt();
+                    int sum = 0;
+                    if (num > 10 && num < 100) {
+                        for (int i = 1; i <= num; i++) {
+                            if (i % 2 != 0) {
+                                sum += i;
+                            }
                         }
+                        System.out.println("Sum = " + sum);
+                    } else {
+                        System.out.println("Please pick another number.");
                     }
-                    System.out.println("Tong so le = " + sum);
-                }
-                else {
-                    System.out.println("Vui long nhap lai");
-                }
-                break;
-            case 2:
-                System.out.print("Nhap so de kiem tra so nguyen to: ");
-                int number = input.nextInt();
-                int i = 2;
-                boolean check = true;
-                while (i <= Math.sqrt(number)) {
-                    if (number % i == 0) {
-                        check = false;
-                        break;
+                    break;
+                case 2:
+                    System.out.print("Pick a number: ");
+                    int number = input.nextInt();
+                    int i = 2;
+                    boolean check = true;
+                    while (i <= Math.sqrt(number)) {
+                        if (number % i == 0) {
+                            check = false;
+                            break;
+                        }
+                        i++;
                     }
-                    i++;
-                }
-                if (check)
-                    System.out.println(number + " la so nguyen to");
-                else
-                    System.out.println(number + " KHONG phai so nguyen to");
-                break;
-            case 3:
-                System.out.print("Nhap so thang: ");
-                int month = input.nextInt();
-                switch (month){
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 8:
-                    case 10:
-                    case 12:
-                        System.out.println("30 ngay");
-                        break;
-                    case 2:
-                        System.out.println("28 hoac 29 ngay");
-                        break;
-                    case 4:
-                    case 6:
-                    case 9:
-                    case 11:
-                        System.out.println("31 ngay");
-                }
-                break;
-        }
+                    if (check)
+                        System.out.println(number + " is prime.");
+                    else
+                        System.out.println(number + " IS NOT prime.");
+                    break;
+                case 3:
+                    System.out.print("Pick a month: ");
+                    int month = input.nextInt();
+                    switch (month) {
+                        case 1:
+                        case 3:
+                        case 5:
+                        case 7:
+                        case 8:
+                        case 10:
+                        case 12:
+                            System.out.println("31 days");
+                            break;
+                        case 2:
+                            System.out.println("28 or 29 days");
+                            break;
+                        case 4:
+                        case 6:
+                        case 9:
+                        case 11:
+                            System.out.println("30 days");
+                            break;
+                        default:
+                            System.out.println("Invalid month");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
 
+            // Reset to select program again
+            System.out.print("Do you want to reset? (Y/N): ");
+            continueChoice = input.next();
 
+        } while (continueChoice.equalsIgnoreCase("Y")); // Reset if user choose Y
+
+        System.out.println("End of program.");
     }
 }
